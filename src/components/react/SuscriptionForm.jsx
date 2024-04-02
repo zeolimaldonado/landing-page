a3eba61550410244ab1d8369eba9ef15f9783c6d
Zeoli Maldonado <zeoli.maldonado@dappis.mx>
2024-04-02 16:56:28 -0600

    Pricing section

--- src/components/react/SuscriptionForm.jsx
+++ src/components/react/SuscriptionForm.jsx
@@ -1,15 +1,19 @@
 
 export default function SuscriptionForm () {
 
+	const handleSubmit = (e) => {
+		e.preventDefault()
+	}
+
     return (
 		<article className='flex-1 px-4 space-y-4'>
-			<h1 class="text-3xl font-bold text-slate-300">Mantente informado</h1>
-			<p class="font-semibold text-sm text-slate-400 text-pretty mb-3">
-				Recibe nuestras actualizaciones en tiempo real a tu correo electrónico.
+			<h1 className="text-3xl font-bold text-slate-300">Mantente informado</h1>
+			<p className="font-semibold text-sm text-slate-400 text-pretty mb-3">
+				Recibe nuestras novedades, promociones y artículos directamente a tu correo electrónico.
 			</p>
-			<form class="space-y-4">
-				<input type="text" class="w-full bg-neutral-600 outline-neutral-700 text-slate-400 text-sm px-3 py-2" placeholder="Correo electrónico" />
-				<button class="w-full rounded-full bg-blue-600 py-2 hover:bg-blue-700">Suscribirme</button>
+			<form onSubmit={handleSubmit} className="space-y-4">
+				<input type="text" className="w-full bg-neutral-600 outline-neutral-700 text-slate-400 text-sm px-3 py-2" placeholder="Correo electrónico" />
+				<button type='submit' className="w-full rounded-full bg-blue-600 py-2 hover:bg-blue-700">Suscribirme</button>
 			</form>
 		</article>
     )
--- src/constants/index.js
+++ src/constants/index.js
@@ -21,3 +21,58 @@ export const navigation = [
         href: '#contact'
     }
 ]
+
+export const PRICING_PLANS = [
+    {
+        title: 'Free',
+        price: '0.00',
+        description: 'For individuals who just want to get started',
+        previous: false,
+        features: [
+            '1GB Storage',
+            '100GB Bandwidth',
+            '5 Email Accounts',
+            '10 Domains',
+            '5 MySQL Databases'
+        ]
+    },
+    {
+        title: 'Basic',
+        price: '10.00',
+        description: 'For individuals who want to scale',
+        previous: true,
+        features: [
+            '5GB Storage',
+            '500GB Bandwidth',
+            '20 Email Accounts',
+            '50 Domains',
+            '20 MySQL Databases'
+        ]
+    },
+    {
+        title: 'Pro',
+        price: '25.00',
+        description: 'For individuals who want to scale',
+        previous: true,
+        features: [
+            '10GB Storage',
+            '1TB Bandwidth',
+            '50 Email Accounts',
+            '100 Domains',
+            '50 MySQL Databases'
+        ]
+    },
+    {
+        title: 'Business',
+        price: '50.00',
+        description: 'For individuals who want to scale',
+        previous: true,
+        features: [
+            '20GB Storage',
+            '2TB Bandwidth',
+            '100 Email Accounts',
+            '200 Domains',
+            '100 MySQL Databases'
+        ]
+    }
+]
