3c796d6f113d06ffafef6450d9dd04df7692fedb
Zeoli Maldonado <zeoli.maldonado@dappis.mx>
2024-04-02 12:50:13 -0600

    add constants

---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+
+export const navigation = [
+    {
+        title: 'Inicio',
+        href: '#home'
+    },
+    {
+        title: 'Características',
+        href: '#features'
+    },
+    {
+        title: 'Precios',
+        href: '#pricing'
+    },
+    {
+        title: 'FAQ',
+        href: '#faq'
+    },
+    {
+        title: 'Contacto',
+        href: '#contact'
+    }
+]
