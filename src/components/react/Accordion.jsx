a6fb6965d8116ed47b5975b2cc22c49f816b0ced
Zeoli Maldonado <zeoli.maldonado@dappis.mx>
2024-04-02 21:01:38 -0600

    Faq section

--- src/components/react/Accordion.jsx
+++ src/components/react/Accordion.jsx
@@ -12,13 +12,15 @@ function AccordionItem ({ question, answer, item }) {
 
     return (
         <article className="bg-neutral-700 rounded-sm flex flex-col gap-0">
-            <div onClick={handleClick} className="px-8 py-6 flex justify-between gap-3 items-center text-slate-300">
+            <div onClick={handleClick} className="px-8 py-6 flex justify-between gap-3 items-center text-slate-300 cursor-pointer">
                 <h2 className="text-xl font-bold">
                     {question}
                 </h2>
-                <FaAngleDown size="24" />
+                <div className={`${context.item === item ? 'rotate-180' : 'rotate-0'}`}>
+                    <FaAngleDown size="24" />
+                </div>
             </div>
-            <div className={`bg-neutral-600 overflow-y-hidden ${context.item === item ? ' max-h-screen' : 'max-h-0'} transition-max-height duration-300`}>
+            <div className={`bg-neutral-600 overflow-y-hidden ${context.item === item ? ' max-h-screen' : 'max-h-0'}`}>
                 <p className="text-slate-300 px-8 py-6 font-medium">
                     {answer}
                 </p>
@@ -28,29 +30,13 @@ function AccordionItem ({ question, answer, item }) {
 }
 
 function AccordionWrapper () {
-    const { list: faq, setItem, item } = useContext(AccordionContext);
-
-    const handleClick = (item) => () => {
-        setItem(item);
-    }
+    const { list: faq } = useContext(AccordionContext);
 
     return (
         <div className="flex-1 flex flex-col gap-4">
             {
                 faq.map(({ question, answer }, index) => (
-                    <article key={index} className="bg-neutral-700 rounded-sm flex flex-col gap-0">
-                        <div onClick={handleClick(index)} className="px-8 py-6 flex justify-between gap-3 items-center text-slate-300">
-                            <h2 className="text-xl font-bold">
-                                {question}
-                            </h2>
-                            <FaAngleDown size="24" />
-                        </div>
-                        <div className={`bg-neutral-600 overflow-y-hidden ${item === index ? ' max-h-screen' : 'max-h-0'} transition-max-height duration-300`}>
-                            <p className="text-slate-300 px-8 py-6 font-medium">
-                                {answer}
-                            </p>
-                        </div>
-                    </article>
+                    <AccordionItem question={question} answer={answer} item={index} key={index} />
                 ))
             }
         </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,6 @@ export default {
 	theme: {
 		extend: {
 			transitionProperty: {
-				'height': 'height',
 				'max-height': 'max-height',
 			}
 		},
