2da7175e89515b289534478bb39e17a4b0d62082
Zeoli Maldonado <zeoli.maldonado@dappis.mx>
2024-04-03 13:54:01 -0600

    Hero floating image

--- src/components/react/ContactForm.jsx
+++ src/components/react/ContactForm.jsx
@@ -9,26 +9,26 @@ export default function ContactForm () {
         <form onSubmit={handleSubmit} className="w-3/5 mx-auto py-4 text-slate-300 space-y-4">
             <div className="flex justify-between items-center gap-4">
                 <article className='flex-1 space-y-3'>
-                    <label for="name">Nombre</label>
+                    <label htmlFor="name">Nombre</label>
                     <input type="text" id="name" className="w-full bg-neutral-700 outline-neutral-700 text-slate-300 text-sm px-3 py-3 rounded-md" placeholder='Nombre completo' />
                 </article>
                 <article className='flex-1 space-y-3'>
-                    <label for="title">Título</label>
+                    <label htmlFor="title">Título</label>
                     <input type="text" id="title" className="w-full bg-neutral-700 outline-neutral-700 text-slate-300 text-sm px-3 py-3 rounded-md" placeholder='Título del mensaje' />
                 </article>
             </div>
             <div className="flex justify-between items-center gap-4">
                 <article className='flex-1 space-y-3'>
-                    <label for="company-email">Correo empresarial</label>
+                    <label htmlFor="company-email">Correo empresarial</label>
                     <input type="text" id="company-email" className="w-full bg-neutral-700 outline-neutral-700 text-slate-300 text-sm px-3 py-3 rounded-md" placeholder='Correo empresarial' />
                 </article>
                 <article className='flex-1 space-y-3'>
-                    <label for="company">Nombre de la empresa</label>
+                    <label htmlFor="company">Nombre de la empresa</label>
                     <input type="text" id="company" className="w-full bg-neutral-700 outline-neutral-700 text-slate-300 text-sm px-3 py-3 rounded-md" placeholder='Nombre de la empresa' />
                 </article>
             </div>
             <div className='space-y-3'>
-                <label for="company-size">Tamaño de la empresa</label>
+                <label htmlFor="company-size">Tamaño de la empresa</label>
                 <div className='flex gap-6 items-center'>
                     <div className='flex gap-2 items-center'>
                         <input type='radio' name='company-size' className='w-4 h-4' />
@@ -53,7 +53,7 @@ export default function ContactForm () {
                 </div>
             </div>
             <div className='space-y-3'>
-                <label for="message">Mensaje</label>
+                <label htmlFor="message">Mensaje</label>
                 <textarea id="message" rows='10' className="w-full bg-neutral-700 outline-neutral-700 text-slate-300 text-sm px-3 py-2 rounded-md resize-none" placeholder='Escribe tu mensaje' />
             </div>
             <div>
