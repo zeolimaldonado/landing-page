13baf6877471997a6382ec30da209fdb4db2ca88
Zeoli Maldonado <zeoli.maldonado@dappis.mx>
2024-04-04 21:01:04 -0600

    Topic change to server services landing page

--- src/constants/index.js
+++ src/constants/index.js
@@ -79,73 +79,81 @@ export const PRICING_PLANS = [
 
 export const FAQ = [
     {
-        question: 'What is Astro?',
-        answer: 'Astro is a new way to build static websites. It is a new way to build static websites. It is a new way to build static websites.'
+        question: '¿Qué es la escalabilidad instantánea y cómo me beneficia?',
+        answer: 'La escalabilidad instantánea te permite ajustar rápidamente los recursos de tu servidor según las necesidades de tu negocio. Esto significa que puedes aumentar o disminuir la capacidad de tu servidor en cuestión de minutos, lo que te permite manejar picos de tráfico sin problemas y optimizar los costos operativos.'
     },
     {
-        question: 'What is Astro?',
-        answer: 'Astro is a new way to build static websites. It is a new way to build static websites. It is a new way to build static websites.'
+        question: '¿Qué medidas de seguridad tienen implementadas en sus servidores?',
+        answer: 'Nuestros servidores están protegidos con medidas de seguridad de grado empresarial, incluyendo firewalls, encriptación de datos, monitoreo constante y actualizaciones de seguridad regulares. Además, ofrecemos opciones de seguridad adicionales personalizadas según tus necesidades específicas.'
     },
     {
-        question: 'What is Astro?',
-        answer: 'Astro is a new way to build static websites. It is a new way to build static websites. It is a new way to build static websites.'
+        question: '¿Cómo puedo obtener soporte técnico en caso de tener algún problema con mi servidor?',
+        answer: 'Ofrecemos soporte técnico experto las 24 horas del día, los 7 días de la semana. Puedes contactar a nuestro equipo de atención al cliente por teléfono, correo electrónico o chat en vivo para recibir asistencia inmediata y resolver cualquier problema que puedas tener con tu servidor.'
     },
     {
-        question: 'What is Astro?',
-        answer: 'Astro is a new way to build static websites. It is a new way to build static websites. It is a new way to build static websites.'
+        question: '¿Cuál es la diferencia entre sus servidores y otras opciones de alojamiento?',
+        answer: 'Nuestros servidores ofrecen un rendimiento óptimo, alta disponibilidad, flexibilidad de configuración y seguridad robusta que no siempre se encuentra en otras opciones de alojamiento. Además, nuestra escalabilidad instantánea te permite ajustar fácilmente los recursos de tu servidor según tus necesidades cambiantes.'
+    },
+    {
+        question: '¿Qué tipo de aplicaciones puedo alojar en sus servidores?',
+        answer: 'Nuestros servidores son adecuados para una amplia gama de aplicaciones, incluyendo sitios web, tiendas en línea, aplicaciones móviles, juegos en línea, sistemas de gestión de contenido (CMS), sistemas de gestión del aprendizaje (LMS) y más. Si tienes alguna duda sobre la compatibilidad de tu aplicación, nuestro equipo de soporte técnico estará encantado de ayudarte.'
+    },
+    {
+        question: '¿Puedo personalizar la configuración de mi servidor según mis necesidades específicas?',
+        answer: 'Sí, nuestra plataforma ofrece flexibilidad de configuración que te permite personalizar los recursos de tu servidor, como la capacidad de almacenamiento, la potencia de procesamiento y la memoria, para adaptarse a tus necesidades específicas. Nuestro equipo de expertos estará encantado de ayudarte a diseñar la configuración perfecta para tu proyecto.'
     }
 ]
 
 export const PEOPLE = [
     {
-        job: 'Guardia de Seguridad',
-        description: 'Cuentan con la aplicación móvil para registrar rondas de inspección, visitas y visitas de seguridad, entre otras actividades.'
+        job: 'Startups',
+        description: 'Empresas en crecimiento que necesitan servidores flexibles para adaptarse rápidamente a sus necesidades cambiantes.'
     },
     {
-        job: 'Guardia de Seguridad',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        job: 'Comercio electrónico',
+        description: 'Empresas que requieren servidores confiables para mantener sus tiendas virtuales disponibles las 24/7, especialmente durante picos de tráfico.'
     },
     {
-        job: 'Guardia de Seguridad',
-        description: 'Cuentan con la aplicación móvil para registrar rondas de inspección, visitas y visitas de seguridad, entre otras actividades.'
+        job: 'Desarrolladores de software',
+        description: 'Profesionales que necesitan entornos de servidor flexibles y eficientes para desarrollar, probar y ejecutar aplicaciones.'
     },
     {
-        job: 'Guardia de Seguridad',
-        description: 'Cuentan con la aplicación móvil para registrar rondas de inspección, visitas y visitas de seguridad, entre otras actividades.'
+        job: 'Agencias digitales',
+        description: 'Empresas que alojan los sitios web de sus clientes y buscan rendimiento superior y seguridad sólida.'
     },
     {
-        job: 'Guardia de Seguridad',
-        description: 'Cuentan con la aplicación móvil para registrar rondas de inspección, visitas y visitas de seguridad, entre otras actividades.'
+        job: 'Instituciones educativas',
+        description: 'Universidades y colegios que utilizan servidores para alojar sistemas de aprendizaje en línea y sitios web institucionales.'
     },
     {
-        job: 'Guardia de Seguridad',
-        description: 'Cuentan con la aplicación móvil para registrar rondas de inspección, visitas y visitas de seguridad, entre otras actividades.'
+        job: 'Aplicaciones móviles y juegos en línea',
+        description: 'Desarrolladores que necesitan servidores potentes y escalables para alojar aplicaciones y juegos con grandes volúmenes de usuarios concurrentes.'
     }
 ]
 
 export const FEATURES = [
     {
-        title: 'Tu información en un solo lugar',
-        description: 'Mantente actualizado con la información de toda la organización en tiempo real.'
+        title: 'Escalabilidad instantánea',
+        description: 'Ajusta rápidamente los recursos de tu servidor según las necesidades de tu negocio, sin interrupciones ni tiempos de inactividad.'
     },
     {
-        title: 'Inteligente',
-        description: 'Mantente actualizado con la información de toda la organización en tiempo real.'
+        title: 'Alta disponibilidad',
+        description: 'Garantiza un funcionamiento ininterrumpido de tus aplicaciones con nuestra infraestructura de servidores altamente confiable.'
     },
     {
-        title: 'Seguro',
-        description: 'Mantente actualizado con la información de toda la organización en tiempo real.'
+        title: 'Seguridad robusta',
+        description: 'Protege tus datos y la integridad de tu negocio con nuestras medidas de seguridad avanzadas, incluyendo firewalls, encriptación y monitoreo constante.'
     },
     {
-        title: 'Flexible',
-        description: 'Mantente actualizado con la información de toda la organización en tiempo real.'
+        title: 'Rendimiento óptimo',
+        description: 'Experimenta un rendimiento excepcional con nuestros servidores de última generación, optimizados para cargas de trabajo exigentes y aplicaciones de alto tráfico.'
     },
     {
-        title: 'Personalizable',
-        description: 'Mantente actualizado con la información de toda la organización en tiempo real.'
+        title: 'Soporte técnico experto',
+        description: 'Obtén asistencia técnica de primer nivel de nuestro equipo de expertos en servidores, disponibles las 24 horas del día, los 7 días de la semana.'
     },
     {
-        title: 'Automatizado',
-        description: 'Mantente actualizado con la información de toda la organización en tiempo real.'
+        title: 'Flexibilidad de configuración',
+        description: 'Personaliza la configuración de tu servidor según tus necesidades específicas, desde la capacidad de almacenamiento hasta la potencia de procesamiento, para maximizar la eficiencia de tus operaciones.'
     },
 ]
